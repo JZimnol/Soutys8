ac339fe70664d47e427763ee057b58d029780030
JZimnol <zimnol@student.agh.edu.pl>
2022-12-13 21:34:52 +0100

    Rename signals from .bde files from BUS* to proper names

--- compile/top.vhd
+++ compile/top.vhd
@@ -191,34 +191,34 @@ end component;
 ---- Signal declarations used on the diagram ----
 
 signal CLK : STD_LOGIC;
-signal NET805 : STD_LOGIC;
-signal BUS1087 : STD_LOGIC_VECTOR(4 downto 0);
-signal BUS1091 : STD_LOGIC_VECTOR(4 downto 0);
-signal BUS1105 : STD_LOGIC_VECTOR(5 downto 0);
-signal BUS1147 : STD_LOGIC_VECTOR(7 downto 0);
-signal BUS1151 : STD_LOGIC_VECTOR(7 downto 0);
-signal BUS1228 : STD_LOGIC_VECTOR(7 downto 0);
-signal BUS1263 : STD_LOGIC_VECTOR(7 downto 0);
-signal BUS1358 : STD_LOGIC_VECTOR(7 downto 0);
-signal BUS1361 : STD_LOGIC_VECTOR(7 downto 0);
-signal BUS1517 : STD_LOGIC_VECTOR(4 downto 0);
+signal s_uart_byte_received : STD_LOGIC;
 signal BUS1738 : STD_LOGIC_VECTOR(15 downto 0);
-signal BUS2126 : STD_LOGIC_VECTOR(7 downto 0);
-signal BUS2135 : STD_LOGIC_VECTOR(15 downto 0);
-signal BUS2918 : STD_LOGIC_VECTOR(7 downto 0);
-signal BUS5174 : STD_LOGIC_VECTOR(7 downto 0);
-signal BUS678 : STD_LOGIC_VECTOR(15 downto 0);
-signal BUS726 : STD_LOGIC_VECTOR(15 downto 0);
 signal BUS740 : STD_LOGIC_VECTOR(15 downto 0);
 signal BUS764 : STD_LOGIC_VECTOR(15 downto 0);
-signal BUS777 : STD_LOGIC_VECTOR(15 downto 0);
-signal BUS7793 : STD_LOGIC_VECTOR(15 downto 0);
-signal BUS7814 : STD_LOGIC_VECTOR(15 downto 0);
-signal BUS814 : STD_LOGIC_VECTOR(7 downto 0);
-signal BUS835 : STD_LOGIC_VECTOR(31 downto 0);
 signal CONST_0 : STD_LOGIC_VECTOR(15 downto 0);
 signal CONST_1 : STD_LOGIC_VECTOR(15 downto 0);
 signal CONTROL : STD_LOGIC_VECTOR(31 downto 0);
+signal s_alu_data : STD_LOGIC_VECTOR(7 downto 0);
+signal s_alu_src1 : STD_LOGIC_VECTOR(7 downto 0);
+signal s_alu_src2 : STD_LOGIC_VECTOR(7 downto 0);
+signal s_branch_address : STD_LOGIC_VECTOR(15 downto 0);
+signal s_current_pc : STD_LOGIC_VECTOR(15 downto 0);
+signal s_data_memory_read_data : STD_LOGIC_VECTOR(7 downto 0);
+signal s_data_memory_write_data : STD_LOGIC_VECTOR(7 downto 0);
+signal s_immediate : STD_LOGIC_VECTOR(7 downto 0);
+signal s_instructions : STD_LOGIC_VECTOR(31 downto 0);
+signal s_instruction_number : STD_LOGIC_VECTOR(15 downto 0);
+signal s_instruction_type : STD_LOGIC_VECTOR(5 downto 0);
+signal s_next_pc : STD_LOGIC_VECTOR(15 downto 0);
+signal s_reg1_addr : STD_LOGIC_VECTOR(4 downto 0);
+signal s_reg1_data : STD_LOGIC_VECTOR(7 downto 0);
+signal s_reg2_addr : STD_LOGIC_VECTOR(4 downto 0);
+signal s_reg2_data : STD_LOGIC_VECTOR(7 downto 0);
+signal s_reg3_addr : STD_LOGIC_VECTOR(4 downto 0);
+signal s_sram_addr : STD_LOGIC_VECTOR(15 downto 0);
+signal s_sreg_flags : STD_LOGIC_VECTOR(7 downto 0);
+signal s_uart_data_address : STD_LOGIC_VECTOR(15 downto 0);
+signal s_uart_incomming_data : STD_LOGIC_VECTOR(7 downto 0);
 
 begin
 
@@ -226,7 +226,7 @@ begin
 
 U10 : MUX2_16bit_branch
   port map(
-       in_input1 => BUS7814,
+       in_input1 => s_branch_address,
        in_input2 => CONST_0,
        out_output => BUS1738,
        in_control => CONTROL(31 downto 0)
@@ -249,24 +249,24 @@ U17 : UART_Rx
        in_clk => CLK_12MHz,
        in_tx_bit => UART_data_in,
        in_reset => RESET,
-       out_rx_data => BUS814(7 downto 0),
-       out_byte_received => NET805,
-       out_data_address => BUS678
+       out_rx_data => s_uart_incomming_data(7 downto 0),
+       out_byte_received => s_uart_byte_received,
+       out_data_address => s_uart_data_address
   );
 
 U18 : UART_Tx
   port map(
        in_clk => CLK_12MHz,
-       in_tx_data => BUS814(7 downto 0),
-       in_begin_send => NET805,
+       in_tx_data => s_uart_incomming_data(7 downto 0),
+       in_begin_send => s_uart_byte_received,
        out_tx_bit => UART_data_out
   );
 
 U19 : MUX2_16bit_addr
   port map(
-       in_input1 => BUS678,
-       in_input2 => BUS777,
-       out_output => BUS7793,
+       in_input1 => s_uart_data_address,
+       in_input2 => s_current_pc,
+       out_output => s_instruction_number,
        in_reset => RESET
   );
 
@@ -274,21 +274,21 @@ U20 : Program_counter
   port map(
        in_clk => CLK,
        in_reset => RESET,
-       in_next_instruction => BUS726,
-       out_current_instruction => BUS777
+       in_next_instruction => s_next_pc,
+       out_current_instruction => s_current_pc
   );
 
 U21 : MUX2_16bit_PC
   port map(
        in_input1 => BUS764,
-       in_input2 => BUS7814,
-       out_output => BUS726,
+       in_input2 => s_branch_address,
+       out_output => s_next_pc,
        in_control => CONTROL(31 downto 0)
   );
 
 U22 : Adder_16bit
   port map(
-       in_input1 => BUS777,
+       in_input1 => s_current_pc,
        in_input2 => BUS1738,
        out_output => BUS740
   );
@@ -302,82 +302,82 @@ U23 : Adder_16bit
 
 U24 : Instruction_decoder
   port map(
-       in_instruction => BUS835,
-       out_instruction_type => BUS1105,
-       out_reg1_addr => BUS1087,
-       out_reg2_addr => BUS1091,
-       out_reg3_addr => BUS1517,
-       out_immediate => BUS1358,
-       out_sram_address => BUS2135,
-       out_branch_address => BUS7814
+       in_instruction => s_instructions,
+       out_instruction_type => s_instruction_type,
+       out_reg1_addr => s_reg1_addr,
+       out_reg2_addr => s_reg2_addr,
+       out_reg3_addr => s_reg3_addr,
+       out_immediate => s_immediate,
+       out_sram_address => s_sram_addr,
+       out_branch_address => s_branch_address
   );
 
 U25 : Program_memory
   port map(
-       in_instruction_number => BUS7793,
+       in_instruction_number => s_instruction_number,
        in_clk => CLK_12MHz,
-       in_prog_trigger => NET805,
-       in_prog_data => BUS814,
-       out_instruction => BUS835
+       in_prog_trigger => s_uart_byte_received,
+       in_prog_data => s_uart_incomming_data,
+       out_instruction => s_instructions
   );
 
 U3 : Data_memory
   port map(
-       in_reg1_addr => BUS1087,
-       in_reg2_addr => BUS1091,
-       in_write_addres => BUS2135,
-       in_write_data => BUS2126,
+       in_reg1_addr => s_reg1_addr,
+       in_reg2_addr => s_reg2_addr,
+       in_write_addres => s_sram_addr,
+       in_write_data => s_data_memory_write_data,
        in_control => CONTROL(31 downto 0),
        in_clk => CLK,
        in_reset => RESET,
-       out_reg1_data => BUS5174,
-       out_reg2_data => BUS1151,
-       out_read_data => BUS2918,
+       out_reg1_data => s_reg1_data,
+       out_reg2_data => s_reg2_data,
+       out_read_data => s_data_memory_read_data,
        io_GPIO => io_GPIO,
-       in_reg3_addr => BUS1517
+       in_reg3_addr => s_reg3_addr
   );
 
 U4 : Control_unit
   port map(
-       in_SREG_flags => BUS1263,
-       in_instruction_type => BUS1105,
+       in_SREG_flags => s_sreg_flags,
+       in_instruction_type => s_instruction_type,
        out_control => CONTROL(31 downto 0)
   );
 
 U5 : ALU
   port map(
-       in_src1 => BUS1228,
-       in_src2 => BUS1147,
-       out_result => BUS1361,
+       in_src1 => s_alu_src1,
+       in_src2 => s_alu_src2,
+       out_result => s_alu_data,
        in_SREG_clk => CLK,
        in_reset => RESET,
        in_control => CONTROL(31 downto 0),
-       out_SREG_flags => BUS1263
+       out_SREG_flags => s_sreg_flags
   );
 
 U6 : MUX2_8bit_src2
   port map(
-       in_input1 => BUS1151,
-       in_input2 => BUS1358,
-       out_output => BUS1147,
+       in_input1 => s_reg2_data,
+       in_input2 => s_immediate,
+       out_output => s_alu_src2,
        in_control => CONTROL(31 downto 0)
   );
 
 U7 : MUX2_8bit_src1
   port map(
-       in_input1 => BUS5174,
-       in_input2 => BUS2918,
-       out_output => BUS1228,
+       in_input1 => s_reg1_data,
+       in_input2 => s_data_memory_read_data,
+       out_output => s_alu_src1,
        in_control => CONTROL(31 downto 0)
   );
 
 U8 : MUX4_8bit
   port map(
-       in_input1 => BUS1358,
-       in_input2 => BUS1151,
-       in_input3 => BUS2918,
-       in_input4 => BUS1361,
-       out_output => BUS2126,
+       in_input1 => s_immediate,
+       in_input2 => s_reg2_data,
+       in_input3 => s_data_memory_read_data,
+       in_input4 => s_alu_data,
+       out_output => s_data_memory_write_data,
        in_control => CONTROL(31 downto 0)
   );
 
