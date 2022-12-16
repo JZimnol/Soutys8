a645aeaa4b6fe4fa39068ecbc53a7ce3053e6ad3
JZimnol <zimnol@student.agh.edu.pl>
2022-12-16 18:05:09 +0100

    add laboratory_version model

--- compile/top.vhd
+++ compile/top.vhd
@@ -188,17 +188,16 @@ component UART_Tx
   );
 end component;
 
+----     Constants     -----
+constant DANGLING_INPUT_CONSTANT : STD_LOGIC := 'Z';
+
 ---- Signal declarations used on the diagram ----
 
 signal CLK : STD_LOGIC;
 signal s_uart_byte_received : STD_LOGIC;
-signal BUS1738 : STD_LOGIC_VECTOR(15 downto 0);
-signal BUS740 : STD_LOGIC_VECTOR(15 downto 0);
-signal BUS764 : STD_LOGIC_VECTOR(15 downto 0);
 signal CONST_0 : STD_LOGIC_VECTOR(15 downto 0);
 signal CONST_1 : STD_LOGIC_VECTOR(15 downto 0);
 signal CONTROL : STD_LOGIC_VECTOR(31 downto 0);
-signal s_alu_data : STD_LOGIC_VECTOR(7 downto 0);
 signal s_alu_src1 : STD_LOGIC_VECTOR(7 downto 0);
 signal s_alu_src2 : STD_LOGIC_VECTOR(7 downto 0);
 signal s_branch_address : STD_LOGIC_VECTOR(15 downto 0);
@@ -220,15 +219,32 @@ signal s_sreg_flags : STD_LOGIC_VECTOR(7 downto 0);
 signal s_uart_data_address : STD_LOGIC_VECTOR(15 downto 0);
 signal s_uart_incomming_data : STD_LOGIC_VECTOR(7 downto 0);
 
+---- Declaration for Dangling input ----
+signal Dangling_Input_Signal : STD_LOGIC;
+
 begin
 
 ----  Component instantiations  ----
 
 U10 : MUX2_16bit_branch
   port map(
-       in_input1 => s_branch_address,
+       in_input1(15) => Dangling_Input_Signal,
+       in_input1(14) => Dangling_Input_Signal,
+       in_input1(13) => Dangling_Input_Signal,
+       in_input1(12) => Dangling_Input_Signal,
+       in_input1(11) => Dangling_Input_Signal,
+       in_input1(10) => Dangling_Input_Signal,
+       in_input1(9) => Dangling_Input_Signal,
+       in_input1(8) => Dangling_Input_Signal,
+       in_input1(7) => Dangling_Input_Signal,
+       in_input1(6) => Dangling_Input_Signal,
+       in_input1(5) => Dangling_Input_Signal,
+       in_input1(4) => Dangling_Input_Signal,
+       in_input1(3) => Dangling_Input_Signal,
+       in_input1(2) => Dangling_Input_Signal,
+       in_input1(1) => Dangling_Input_Signal,
+       in_input1(0) => Dangling_Input_Signal,
        in_input2 => CONST_0,
-       out_output => BUS1738,
        in_control => CONTROL(31 downto 0)
   );
 
@@ -265,7 +281,22 @@ U18 : UART_Tx
 U19 : MUX2_16bit_addr
   port map(
        in_input1 => s_uart_data_address,
-       in_input2 => s_current_pc,
+       in_input2(15) => Dangling_Input_Signal,
+       in_input2(14) => Dangling_Input_Signal,
+       in_input2(13) => Dangling_Input_Signal,
+       in_input2(12) => Dangling_Input_Signal,
+       in_input2(11) => Dangling_Input_Signal,
+       in_input2(10) => Dangling_Input_Signal,
+       in_input2(9) => Dangling_Input_Signal,
+       in_input2(8) => Dangling_Input_Signal,
+       in_input2(7) => Dangling_Input_Signal,
+       in_input2(6) => Dangling_Input_Signal,
+       in_input2(5) => Dangling_Input_Signal,
+       in_input2(4) => Dangling_Input_Signal,
+       in_input2(3) => Dangling_Input_Signal,
+       in_input2(2) => Dangling_Input_Signal,
+       in_input2(1) => Dangling_Input_Signal,
+       in_input2(0) => Dangling_Input_Signal,
        out_output => s_instruction_number,
        in_reset => RESET
   );
@@ -280,8 +311,38 @@ U20 : Program_counter
 
 U21 : MUX2_16bit_PC
   port map(
-       in_input1 => BUS764,
-       in_input2 => s_branch_address,
+       in_input1(15) => Dangling_Input_Signal,
+       in_input1(14) => Dangling_Input_Signal,
+       in_input1(13) => Dangling_Input_Signal,
+       in_input1(12) => Dangling_Input_Signal,
+       in_input1(11) => Dangling_Input_Signal,
+       in_input1(10) => Dangling_Input_Signal,
+       in_input1(9) => Dangling_Input_Signal,
+       in_input1(8) => Dangling_Input_Signal,
+       in_input1(7) => Dangling_Input_Signal,
+       in_input1(6) => Dangling_Input_Signal,
+       in_input1(5) => Dangling_Input_Signal,
+       in_input1(4) => Dangling_Input_Signal,
+       in_input1(3) => Dangling_Input_Signal,
+       in_input1(2) => Dangling_Input_Signal,
+       in_input1(1) => Dangling_Input_Signal,
+       in_input1(0) => Dangling_Input_Signal,
+       in_input2(15) => Dangling_Input_Signal,
+       in_input2(14) => Dangling_Input_Signal,
+       in_input2(13) => Dangling_Input_Signal,
+       in_input2(12) => Dangling_Input_Signal,
+       in_input2(11) => Dangling_Input_Signal,
+       in_input2(10) => Dangling_Input_Signal,
+       in_input2(9) => Dangling_Input_Signal,
+       in_input2(8) => Dangling_Input_Signal,
+       in_input2(7) => Dangling_Input_Signal,
+       in_input2(6) => Dangling_Input_Signal,
+       in_input2(5) => Dangling_Input_Signal,
+       in_input2(4) => Dangling_Input_Signal,
+       in_input2(3) => Dangling_Input_Signal,
+       in_input2(2) => Dangling_Input_Signal,
+       in_input2(1) => Dangling_Input_Signal,
+       in_input2(0) => Dangling_Input_Signal,
        out_output => s_next_pc,
        in_control => CONTROL(31 downto 0)
   );
@@ -289,15 +350,43 @@ U21 : MUX2_16bit_PC
 U22 : Adder_16bit
   port map(
        in_input1 => s_current_pc,
-       in_input2 => BUS1738,
-       out_output => BUS740
+       in_input2(15) => Dangling_Input_Signal,
+       in_input2(14) => Dangling_Input_Signal,
+       in_input2(13) => Dangling_Input_Signal,
+       in_input2(12) => Dangling_Input_Signal,
+       in_input2(11) => Dangling_Input_Signal,
+       in_input2(10) => Dangling_Input_Signal,
+       in_input2(9) => Dangling_Input_Signal,
+       in_input2(8) => Dangling_Input_Signal,
+       in_input2(7) => Dangling_Input_Signal,
+       in_input2(6) => Dangling_Input_Signal,
+       in_input2(5) => Dangling_Input_Signal,
+       in_input2(4) => Dangling_Input_Signal,
+       in_input2(3) => Dangling_Input_Signal,
+       in_input2(2) => Dangling_Input_Signal,
+       in_input2(1) => Dangling_Input_Signal,
+       in_input2(0) => Dangling_Input_Signal
   );
 
 U23 : Adder_16bit
   port map(
-       in_input1 => BUS740,
-       in_input2 => CONST_1,
-       out_output => BUS764
+       in_input1(15) => Dangling_Input_Signal,
+       in_input1(14) => Dangling_Input_Signal,
+       in_input1(13) => Dangling_Input_Signal,
+       in_input1(12) => Dangling_Input_Signal,
+       in_input1(11) => Dangling_Input_Signal,
+       in_input1(10) => Dangling_Input_Signal,
+       in_input1(9) => Dangling_Input_Signal,
+       in_input1(8) => Dangling_Input_Signal,
+       in_input1(7) => Dangling_Input_Signal,
+       in_input1(6) => Dangling_Input_Signal,
+       in_input1(5) => Dangling_Input_Signal,
+       in_input1(4) => Dangling_Input_Signal,
+       in_input1(3) => Dangling_Input_Signal,
+       in_input1(2) => Dangling_Input_Signal,
+       in_input1(1) => Dangling_Input_Signal,
+       in_input1(0) => Dangling_Input_Signal,
+       in_input2 => CONST_1
   );
 
 U24 : Instruction_decoder
@@ -323,9 +412,32 @@ U25 : Program_memory
 
 U3 : Data_memory
   port map(
-       in_reg1_addr => s_reg1_addr,
-       in_reg2_addr => s_reg2_addr,
-       in_write_addres => s_sram_addr,
+       in_reg1_addr(4) => Dangling_Input_Signal,
+       in_reg1_addr(3) => Dangling_Input_Signal,
+       in_reg1_addr(2) => Dangling_Input_Signal,
+       in_reg1_addr(1) => Dangling_Input_Signal,
+       in_reg1_addr(0) => Dangling_Input_Signal,
+       in_reg2_addr(4) => Dangling_Input_Signal,
+       in_reg2_addr(3) => Dangling_Input_Signal,
+       in_reg2_addr(2) => Dangling_Input_Signal,
+       in_reg2_addr(1) => Dangling_Input_Signal,
+       in_reg2_addr(0) => Dangling_Input_Signal,
+       in_write_addres(15) => Dangling_Input_Signal,
+       in_write_addres(14) => Dangling_Input_Signal,
+       in_write_addres(13) => Dangling_Input_Signal,
+       in_write_addres(12) => Dangling_Input_Signal,
+       in_write_addres(11) => Dangling_Input_Signal,
+       in_write_addres(10) => Dangling_Input_Signal,
+       in_write_addres(9) => Dangling_Input_Signal,
+       in_write_addres(8) => Dangling_Input_Signal,
+       in_write_addres(7) => Dangling_Input_Signal,
+       in_write_addres(6) => Dangling_Input_Signal,
+       in_write_addres(5) => Dangling_Input_Signal,
+       in_write_addres(4) => Dangling_Input_Signal,
+       in_write_addres(3) => Dangling_Input_Signal,
+       in_write_addres(2) => Dangling_Input_Signal,
+       in_write_addres(1) => Dangling_Input_Signal,
+       in_write_addres(0) => Dangling_Input_Signal,
        in_write_data => s_data_memory_write_data,
        in_control => CONTROL(31 downto 0),
        in_clk => CLK,
@@ -334,13 +446,29 @@ U3 : Data_memory
        out_reg2_data => s_reg2_data,
        out_read_data => s_data_memory_read_data,
        io_GPIO => io_GPIO,
-       in_reg3_addr => s_reg3_addr
+       in_reg3_addr(4) => Dangling_Input_Signal,
+       in_reg3_addr(3) => Dangling_Input_Signal,
+       in_reg3_addr(2) => Dangling_Input_Signal,
+       in_reg3_addr(1) => Dangling_Input_Signal,
+       in_reg3_addr(0) => Dangling_Input_Signal
   );
 
 U4 : Control_unit
   port map(
-       in_SREG_flags => s_sreg_flags,
-       in_instruction_type => s_instruction_type,
+       in_SREG_flags(7) => Dangling_Input_Signal,
+       in_SREG_flags(6) => Dangling_Input_Signal,
+       in_SREG_flags(5) => Dangling_Input_Signal,
+       in_SREG_flags(4) => Dangling_Input_Signal,
+       in_SREG_flags(3) => Dangling_Input_Signal,
+       in_SREG_flags(2) => Dangling_Input_Signal,
+       in_SREG_flags(1) => Dangling_Input_Signal,
+       in_SREG_flags(0) => Dangling_Input_Signal,
+       in_instruction_type(5) => Dangling_Input_Signal,
+       in_instruction_type(4) => Dangling_Input_Signal,
+       in_instruction_type(3) => Dangling_Input_Signal,
+       in_instruction_type(2) => Dangling_Input_Signal,
+       in_instruction_type(1) => Dangling_Input_Signal,
+       in_instruction_type(0) => Dangling_Input_Signal,
        out_control => CONTROL(31 downto 0)
   );
 
@@ -348,7 +476,6 @@ U5 : ALU
   port map(
        in_src1 => s_alu_src1,
        in_src2 => s_alu_src2,
-       out_result => s_alu_data,
        in_SREG_clk => CLK,
        in_reset => RESET,
        in_control => CONTROL(31 downto 0),
@@ -357,26 +484,82 @@ U5 : ALU
 
 U6 : MUX2_8bit_src2
   port map(
-       in_input1 => s_reg2_data,
-       in_input2 => s_immediate,
+       in_input1(7) => Dangling_Input_Signal,
+       in_input1(6) => Dangling_Input_Signal,
+       in_input1(5) => Dangling_Input_Signal,
+       in_input1(4) => Dangling_Input_Signal,
+       in_input1(3) => Dangling_Input_Signal,
+       in_input1(2) => Dangling_Input_Signal,
+       in_input1(1) => Dangling_Input_Signal,
+       in_input1(0) => Dangling_Input_Signal,
+       in_input2(7) => Dangling_Input_Signal,
+       in_input2(6) => Dangling_Input_Signal,
+       in_input2(5) => Dangling_Input_Signal,
+       in_input2(4) => Dangling_Input_Signal,
+       in_input2(3) => Dangling_Input_Signal,
+       in_input2(2) => Dangling_Input_Signal,
+       in_input2(1) => Dangling_Input_Signal,
+       in_input2(0) => Dangling_Input_Signal,
        out_output => s_alu_src2,
        in_control => CONTROL(31 downto 0)
   );
 
 U7 : MUX2_8bit_src1
   port map(
-       in_input1 => s_reg1_data,
-       in_input2 => s_data_memory_read_data,
+       in_input1(7) => Dangling_Input_Signal,
+       in_input1(6) => Dangling_Input_Signal,
+       in_input1(5) => Dangling_Input_Signal,
+       in_input1(4) => Dangling_Input_Signal,
+       in_input1(3) => Dangling_Input_Signal,
+       in_input1(2) => Dangling_Input_Signal,
+       in_input1(1) => Dangling_Input_Signal,
+       in_input1(0) => Dangling_Input_Signal,
+       in_input2(7) => Dangling_Input_Signal,
+       in_input2(6) => Dangling_Input_Signal,
+       in_input2(5) => Dangling_Input_Signal,
+       in_input2(4) => Dangling_Input_Signal,
+       in_input2(3) => Dangling_Input_Signal,
+       in_input2(2) => Dangling_Input_Signal,
+       in_input2(1) => Dangling_Input_Signal,
+       in_input2(0) => Dangling_Input_Signal,
        out_output => s_alu_src1,
        in_control => CONTROL(31 downto 0)
   );
 
 U8 : MUX4_8bit
   port map(
-       in_input1 => s_immediate,
-       in_input2 => s_reg2_data,
-       in_input3 => s_data_memory_read_data,
-       in_input4 => s_alu_data,
+       in_input1(7) => Dangling_Input_Signal,
+       in_input1(6) => Dangling_Input_Signal,
+       in_input1(5) => Dangling_Input_Signal,
+       in_input1(4) => Dangling_Input_Signal,
+       in_input1(3) => Dangling_Input_Signal,
+       in_input1(2) => Dangling_Input_Signal,
+       in_input1(1) => Dangling_Input_Signal,
+       in_input1(0) => Dangling_Input_Signal,
+       in_input2(7) => Dangling_Input_Signal,
+       in_input2(6) => Dangling_Input_Signal,
+       in_input2(5) => Dangling_Input_Signal,
+       in_input2(4) => Dangling_Input_Signal,
+       in_input2(3) => Dangling_Input_Signal,
+       in_input2(2) => Dangling_Input_Signal,
+       in_input2(1) => Dangling_Input_Signal,
+       in_input2(0) => Dangling_Input_Signal,
+       in_input3(7) => Dangling_Input_Signal,
+       in_input3(6) => Dangling_Input_Signal,
+       in_input3(5) => Dangling_Input_Signal,
+       in_input3(4) => Dangling_Input_Signal,
+       in_input3(3) => Dangling_Input_Signal,
+       in_input3(2) => Dangling_Input_Signal,
+       in_input3(1) => Dangling_Input_Signal,
+       in_input3(0) => Dangling_Input_Signal,
+       in_input4(7) => Dangling_Input_Signal,
+       in_input4(6) => Dangling_Input_Signal,
+       in_input4(5) => Dangling_Input_Signal,
+       in_input4(4) => Dangling_Input_Signal,
+       in_input4(3) => Dangling_Input_Signal,
+       in_input4(2) => Dangling_Input_Signal,
+       in_input4(1) => Dangling_Input_Signal,
+       in_input4(0) => Dangling_Input_Signal,
        out_output => s_data_memory_write_data,
        in_control => CONTROL(31 downto 0)
   );
@@ -388,5 +571,9 @@ U9 : Reset_LED
        green_led_2 => green_led_2
   );
 
+
+---- Dangling input signal assignment ----
+
+Dangling_Input_Signal <= DANGLING_INPUT_CONSTANT;
 
 end top;
